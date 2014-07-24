56f7ca82765ed0eaaf0415fc72fafc1831714561
Ben West <bewest@gmail.com>
2014-07-24 02:36:22 -0700

    add few more tests for streams

--- tests/test_streams.js
+++ tests/test_streams.js
@@ -6,7 +6,7 @@ var es = require('event-stream');
 describe('sgvdata', function ( ) {
   var fix = {
     "sgv": "123",
-    "device": "test",
+    "device": "dexcom",
     "direction": "",
     "dateString": "2014-07-14T18:19:01-0700",
     "date": 1405387141
@@ -14,13 +14,13 @@ describe('sgvdata', function ( ) {
 
   var fixable = {
     "sgv": 123,
-    "device": "test",
+    "device": "dexcom",
     "direction": "",
     "dateString": "2014-07-14T18:19:01-0700",
     "date": "1405387141"
   };
 
-  var tab = '2014-07-14T18:19:01-0700\t1405387141\t123\t\ttest';
+  var tab = '2014-07-14T18:19:01-0700\t1405387141\t123\t\tdexcom';
   var sgvdata = require('../');
 
 
@@ -67,5 +67,27 @@ describe('sgvdata', function ( ) {
       done( );
     }
     es.pipeline(input, proc, es.writeArray(finish));
+  });
+
+  it('should stream protobufs', function (done) {
+    var input = es.readArray([fix]);
+    var proc = sgvdata.fmt_protobuf( );
+    var hex = '0a18323031342d30372d31355430313a31393a30312e3030305a107b18002001';
+
+    function iter (item, next) {
+      item.toString('hex').should.equal(hex);
+      return next(null, item);
+    }
+    var back = sgvdata.parse_protobuf( );
+
+    function finish (err, results) {
+      results[0].device.should.equal('dexcom');
+      results[0].direction.should.equal('NONE');
+      results[0].dateString.should.equal('2014-07-15T01:19:01.000Z');
+      results[0].date.should.equal(fix.date);
+      results[0].sgv.toString( ).should.equal(fix.sgv);
+      done( );
+    }
+    es.pipeline(input, proc, es.map(iter), back, es.writeArray(finish));
   });
 });
