0b8c9e475d30d802decc862cf6634e8384bcc1a4
PieterGit <pieterg@gmail.com>
2018-07-22 16:49:35 +0200

    add test

--- tests/test_protobuf.js
+++ tests/test_protobuf.js
@@ -39,5 +39,13 @@ describe('lib/protobuf', function ( ) {
 
   });
 
+it('should parse protobuffers', function () {
+    // based on Nightscout tests/mqtt.test.js
+    var pb = require('../').sync.protobuf;
+    var payload = new Buffer('0a1108b70110d6d1fa6318f08df963200428011a1d323031352d30382d32335432323a35333a35352e3634392d30373a303020d7d1fa6328004a1508e0920b10c0850b18b20120d5d1fa6328ef8df963620a534d34313837393135306a053638393250', 'hex');
+    var result = pb.parse(payload);
+    result.direction.should.equal('NONE');
+});
+
 });
 
